5589b7a9dc88255e78f745811186c54a1a4e6bb4
seo135 <soongjun3@naver.com>
2022-06-21 10:38:34 +0900

    두번째 commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 import myapp.views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myapp.views.home, name='home'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
